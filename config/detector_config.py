a0297130af5e2fc909006dff44f1f428eae000f8
zhuyuetong <939772941@qq.com>
2025-07-12 00:15:56 +0800

    update

--- config/detector_config.py
+++ config/detector_config.py
@@ -10,7 +10,6 @@ detectors = []
 
 detectors.append(de.DetectorModule(25, (0, 0, 0)))
 detectors += de.Set_Detector_Module(25, 12.5, 12.5, 15)
-detectors += de.Set_Detector_Module(25, 25, 25, 20)
 detectors += de.Set_Detector_Module(25, 37.5, 37.5, 25)    
 detectors += de.Set_Detector_Module(25, 50, 50, 35)
 detectors += de.Set_Detector_Module(25, 75, 75, 50)
@@ -27,5 +26,5 @@ detectors += de.Set_Detector_Module(50, 150, 150, 100)
 #detectors += de.Set_Detector_Module(50, 50, 50, 35)
 #detectors += de.Set_Detector_Module(25, 75, 75, 50)
 #detectors += de.Set_Detector_Module(50, 100, 100, 70)
-#detectors += de.Set_Detector_Module(100, 300, 300, 100)
+#detectors += de.Set_Detector_Module(100, 200, 200, 100)
 
